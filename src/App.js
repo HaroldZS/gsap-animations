e53f775ddacf1ba38d58e7c80b94849e1a3f4bac
HaroldZS <haroldzs1024@gmail.com>
2024-02-05 08:50:17 -0400

    refactor: Implemented Navigation component enhancement

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { Animation2 } from "./routes/Animation2/Animation2";
 import { Animation3 } from "./routes/Animation3/Animation3";
 import { Animation4 } from "./routes/Animation4/Animation4";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import { Navigation } from "./components/Navigation";
+import { Navigation } from "./components/Navigation/Navigation";
 import { Animation1 } from "./routes/Animation1/Animation1";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Routes>
-        <Route path="/" element={<Navigation />} />
+        <Route path="/" element={<p>Home</p>} />
         <Route path="/animation1" element={<Animation1 />} />
         <Route path="/animation2" element={<Animation2 />} />
         <Route path="/animation3" element={<Animation3 />} />
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import "./Navigation.scss";
+
+function Navigation() {
+  return (
+    <nav className="side-navigation">
+      <ul>
+        <li>
+          <Link to="/" className="nav-link">
+            <div className="nav-circle">
+              <AiFillHome />
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/animation1" className="nav-link">
+            <div className="nav-circle">1</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/animation2" className="nav-link">
+            <div className="nav-circle">2</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/animation3" className="nav-link">
+            <div className="nav-circle">3</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/animation4" className="nav-link">
+            <div className="nav-circle">4</div>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export { Navigation };
